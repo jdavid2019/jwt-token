3098dd4aaaf6a6e322ef7cae9ad4b02903151f97
jdavid2019 <joseph2019d@gmail.com>
2021-08-01 23:31:02 -0500

    add jwt token files

---
+++ app/Http/Controllers/TokensController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Validator;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class TokensController extends Controller
+{
+
+    /**
+     * Check if is a valid or invalid user
+     *
+     * @param Request $request
+     */
+    public function login(Request $request) {
+        $credentials = $request->only('email','password');
+        $validator = Validator::make($credentials, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validator -> fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Wrong validation',
+                'errors' =>   $validator->errors()
+            ], 422);
+        }
+        // Obtains a token related with a credentials
+        $token = JWTAuth::attempt($credentials);
+
+        if($token) {
+            return response()->json([
+                'success' => true,
+                'message' => 'OK',
+                'token' => $token,
+                'user' => User::where('email', $credentials['email'])->get()->first(),
+            ], 200);
+        }else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Wrong credentials',
+                'errors' =>   $validator->errors()
+            ], 401);
+        }
+    }
+
+    /**
+     * It can refresh old token to a new
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function refreshToken() {
+        $token = JWTAuth::getToken();
+        try {
+            $token = JWTAuth::refresh($token);
+            return response()->json([
+                'success' => true,
+                'message' => 'OK',
+                'token' => $token,
+            ], 200);
+        } catch (TokenExpiredException $ex){
+            return response()->json([
+                'success' => false,
+                'message' => 'Token expirado',
+            ], 422);
+        } catch (TokenBlacklistedException $ex){
+            return response()->json([
+                'success' => false,
+                'message' => 'Need to login again please (black listed
+                )',
+            ], 422);
+        }
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::group(['middleware' => ['jwt.auth'], 'prefix' => 'v1'], function () {
+
+});
+
+//  api/v1/auth/login
+Route::group(['middleware' => [], 'prefix' => 'v1'], function () {
+    Route::post('/auth/login','App\Http\Controllers\TokensController@login');
+    Route::post('/auth/refresh','App\Http\Controllers\TokensController@refreshToken');
+    // una ruta del expirar token, es un logout
+    Route::get('/auth/expire','App\Http\Controllers\TokensController@expireToken');
+});
+
